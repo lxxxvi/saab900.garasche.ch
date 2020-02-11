d11ddda98f3dc0929e25b4f3102c0cf30ba91c2c
lxxxvi <lxxxvi@users.noreply.github.com>
2020-02-11 09:06:19 +0100

    uploads controller test

--- test/controllers/uploads_controller_test.rb
+++ test/controllers/uploads_controller_test.rb
@@ -2,40 +2,42 @@ require 'test_helper'
 
 class UploadsControllerTest < ActionDispatch::IntegrationTest
   test 'uploads image' do
-    image = fixture_file_upload('files/image.png')
-
-    assert_difference -> { Upload.count }, 1 do
-      post uploads_path, params: { upload: { file: image } }
-    end
-
-    follow_redirect!
-    assert_response :success
-    assert_equal 'Upload erfolgreich', flash[:success]
+    upload_file(fixture_file_upload('files/image.png'), 1)
+    assert_success
   end
 
   test 'uploads pdf' do
-    image = fixture_file_upload('files/pdf.pdf')
+    upload_file(fixture_file_upload('files/pdf.pdf'), 1)
+    assert_success
+  end
 
-    assert_difference -> { Upload.count }, 1 do
-      post uploads_path, params: { upload: { file: image } }
+  test 'uploads nothing' do
+    upload_file(nil, 0)
+    assert_alert
+  end
+
+  test 'uploads unsupported file format' do
+    upload_file(fixture_file_upload('files/docx.docx'), 0)
+    assert_alert
+  end
+
+  private
+
+  def upload_file(file, expected_difference)
+    assert_difference -> { Upload.count }, expected_difference do
+      post uploads_path, params: { upload: { file: file } }
     end
+  end
 
+  def assert_success
     follow_redirect!
     assert_response :success
     assert_equal 'Upload erfolgreich', flash[:success]
   end
 
-  test 'uploads nothing' do
-    assert_no_difference -> { Upload.count } do
-      post uploads_path, params: { upload: { file: nil } }
-    end
-
+  def assert_alert
     follow_redirect!
     assert_response :success
     assert_equal 'Irgendwas hat nicht geklappt, versuchs nochmal ;-)', flash[:alert]
-  end
-
-  test 'uploads unsupported file format' do
-    assert false
   end
 end
